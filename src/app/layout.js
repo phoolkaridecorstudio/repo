673800e8305ab038ffc796b277e5674fed80b9b3
Shivam Aggarwal <shivamaggofficial@gmail.com>
2026-05-31 12:37:57 +0530

    fix(analytics): fix syntax error in script backticks

--- src/app/layout.js
+++ src/app/layout.js
@@ -92,12 +92,12 @@ export default function RootLayout({ children }) {
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
-          {\`
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-51HBDGPQR6');
-          \`}
+          `}
         </Script>
         <script
           type="application/ld+json"
